c2248fc6fa87b6bda4468382a3e8cbc1c41c7234
jen <jboucher154@gmail.com>
2025-11-07 10:15:40 +0200

    chore: add intial db files ticket: DA-4

---
+++ backend/database/base.py
@@ -0,0 +1,47 @@
+from abc import abstractmethod
+import datetime
+from typing import List
+from pydantic import BaseModel
+from backend.measurement.models import MeasurementCreate, MeasurementUpdate
+from backend.measurement.models import MeasurementType
+
+
+class Datastore(BaseModel):
+    """
+    Datastore is the abstract base class for the database and defines the
+    methods required.
+    """
+
+    @abstractmethod
+    async def get_measurement_by_id(id: int):
+        pass
+
+    @abstractmethod
+    async def create_measurement(measuremnt: MeasurementCreate):
+        pass
+
+    @abstractmethod
+    async def update_measurement(measurement: MeasurementUpdate):
+        pass
+
+    @abstractmethod
+    async def delete_measurement(id: int):
+        pass
+
+    @abstractmethod
+    async def get_all_measuremnts_for_day(date: datetime):
+        pass
+
+    @abstractmethod
+    async def bulk_entry_of_measuremnts(
+        measurementes: List[MeasurementCreate]
+    ):
+        pass
+
+    @abstractmethod
+    async def bulk_update_of_measuremnts(updates: List[MeasurementUpdate]):
+        pass
+
+    @abstractmethod
+    async def delete_all_measuremnts_of_a_type(type: MeasurementType):
+        pass
