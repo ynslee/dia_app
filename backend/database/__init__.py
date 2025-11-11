b7391f9a400876f5ddad70ef25fb308c10a38f6f
jen <jboucher154@gmail.com>
2025-11-11 13:28:43 +0200

    fix: linting errors incluidng docstrings

---
+++ backend/database/__init__.py
@@ -0,0 +1,3 @@
+"""
+Contains abstract Base class along with all MockDB and database specific logic.
+"""
--- backend/database/base.py
+++ backend/database/base.py
@@ -13,35 +13,37 @@ class Datastore(BaseModel):
     """
 
     @abstractmethod
-    async def get_measurement_by_id(id: int):
+    async def get_measurement_by_id(self, id: int):
         pass
 
     @abstractmethod
-    async def create_measurement(measuremnt: MeasurementCreate):
+    async def create_measurement(self, measuremnt: MeasurementCreate):
         pass
 
     @abstractmethod
-    async def update_measurement(measurement: MeasurementUpdate):
+    async def update_measurement(self, measurement: MeasurementUpdate):
         pass
 
     @abstractmethod
-    async def delete_measurement(id: int):
+    async def delete_measurement(self, id: int):
         pass
 
     @abstractmethod
-    async def get_all_measuremnts_for_day(date: datetime):
+    async def get_all_measuremnts_for_day(self, date: datetime):
         pass
 
     @abstractmethod
     async def bulk_entry_of_measuremnts(
+        self,
         measurementes: List[MeasurementCreate]
     ):
         pass
 
     @abstractmethod
-    async def bulk_update_of_measuremnts(updates: List[MeasurementUpdate]):
+    async def bulk_update_of_measuremnts(self,
+                                         updates: List[MeasurementUpdate]):
         pass
 
     @abstractmethod
-    async def delete_all_measuremnts_of_a_type(type: MeasurementType):
+    async def delete_all_measuremnts_of_a_type(self, type: MeasurementType):
         pass
