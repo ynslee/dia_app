7994dab1f993e23d88229ba7df4d48b8ed922de3
jen <jboucher154@gmail.com>
2025-11-12 12:41:05 +0200

    fix: formatting of signature and add return type hinting

--- backend/routers/measurement.py
+++ backend/routers/measurement.py
@@ -3,7 +3,7 @@
 
 from database.base import Datastore
 from database.mockdb import open_db_sesssion
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Response
 from fastapi.responses import JSONResponse
 from models.measurement import MeasurementRead
 
@@ -18,8 +18,10 @@ router = APIRouter(
     path="/{item_id}",
     response_model=MeasurementRead
     )
-async def get_mesurement(item_id: int,
-                         db: Datastore = Depends(open_db_sesssion)):
+async def get_mesurement(
+    item_id: int,
+    db: Datastore = Depends(open_db_sesssion)
+    ) -> Response:
     """
     get_measurement uses GET method and returns mesurement with id given as a
     path parameter.
