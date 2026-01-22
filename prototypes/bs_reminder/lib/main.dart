fd2097c655ff2b7fec66ee550698e883b8f5826d
Jenny Boucher <jboucher154@gmail.com>
2026-01-22 13:22:35 +0200

    feat: add take photo button to main scaffold for testing

--- prototypes/bs_reminder/lib/main.dart
+++ prototypes/bs_reminder/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:timezone/data/latest.dart' as tzdata;// database initialization
 import 'package:flutter_timezone/flutter_timezone.dart';//to get device timezone
 import 'services/hc_meal_schedule_service.dart';//replace with actual
 import 'services/meal_reminder_service.dart';//local file
+import 'package:bs_reminder/widgets/photo_button_widget.dart';
 
 
 
@@ -103,18 +104,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // void _incrementCounter() {
+  //   setState(() {
+  //     // This call to setState tells the Flutter framework that something has
+  //     // changed in this State, which causes it to rerun the build method below
+  //     // so that the display can reflect the updated values. If we changed
+  //     // _counter without calling setState(), then the build method would not be
+  //     // called again, and so nothing would appear to happen.
+  //     _counter++;
+  //   });
+  // }
 
   @override
   void initState() {
@@ -152,26 +153,17 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            // testing out photo picking button
+            PhotoButton(onPhotoPicked: (file) {
+              if (file != null) {
+                print('Photo picked: ${file.path}');
+              } else {
+                print('No photo selected');
+              }
+            }
+            )
           ],
         ),
       ),
